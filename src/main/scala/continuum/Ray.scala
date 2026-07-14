884c80465c6b63e855a471cbbb85d118ffc718ae
Denny Crane <denis.zhuravlov@gmail.com>
2026-07-14 15:23:00 -0300

    refactor: implement ray geometry once for both directions
    GreaterRay and LesserRay duplicated apply/encloses/intersects/tangents as
    mirror images with flipped comparisons. Implement them once in the sealed
    Ray class against a direction-oriented ordering (natural for GreaterRay,
    reversed for LesserRay), with respect to which every ray is bounded below
    and points towards larger values.
    
    Subclasses keep only what genuinely differs: tangent, isSameDirection,
    compare, toString. The unreachable AssertionError branches are gone, and
    the shared comparisons call the Ordering directly (no wrapper allocation).

--- src/main/scala/continuum/Ray.scala
+++ src/main/scala/continuum/Ray.scala
@@ -1,7 +1,5 @@
 package continuum
 
-import scala.math.Ordering.Implicits._
-
 import continuum.bound.{Closed, Open, Unbounded}
 
 /**
@@ -12,6 +10,10 @@ import continuum.bound.{Closed, Open, Unbounded}
  * bounded above. A ray's bound can potentially be unbounded, in which case the ray is equivalent to
  * a line.
  *
+ * The geometric operations are implemented once for both directions: each direction supplies the
+ * element ordering oriented along the ray via `directed`, with respect to which every ray is
+ * bounded below and points towards larger values.
+ *
  * @tparam T type of values contained in the continuous, infinite, total-ordered set which the
  *           ray operates on.
  */
@@ -20,27 +22,56 @@ sealed abstract class Ray[T] extends (T => Boolean) {
   def bound: Bound[T]
 
   /**
+   * The element ordering oriented in this ray's direction: the natural ordering for a
+   * `GreaterRay`, the reversed ordering for a `LesserRay`.
+   */
+  protected def directed: Ordering[T]
+
+  /**
    * Tests if this ray contains the specified point.
    */
-  override def apply(point: T): Boolean
+  override def apply(point: T): Boolean = bound match {
+    case Closed(value) => directed.gteq(point, value)
+    case Open(value)   => directed.gt(point, value)
+    case Unbounded()   => true
+  }
 
   /**
    * Tests if this ray encloses the other. A ray encloses another if it contains all points
    * contained by the other.
    */
-  def encloses(other: Ray[T]): Boolean
+  def encloses(other: Ray[T]): Boolean =
+    if (bound.isUnbounded) true
+    else if (isSameDirection(other)) bound.isBelow(other.bound)(directed)
+    else false
 
   /**
    * Tests if this ray intersects the other. Rays intersect if they share any points in common.
    * Said another way, rays intersect if they overlap.
    */
-  def intersects(other: Ray[T]): Boolean
+  def intersects(other: Ray[T]): Boolean =
+    if (bound.isUnbounded || other.bound.isUnbounded) true
+    else if (isSameDirection(other)) true
+    else (bound, other.bound) match {
+      case (Closed(a), Closed(b)) => directed.lteq(a, b)
+      case (Closed(a), Open(b))   => directed.lt(a, b)
+      case (Open(a), Closed(b))   => directed.lt(a, b)
+      case (Open(a), Open(b))     => directed.lt(a, b)
+      case _                      => true
+    }
 
   /**
    * Tests if this ray is tangent to the other. Rays are tangent if they do not contain
    * any points in common, but all points are contained by one of the rays.
    */
-  def tangents(other: Ray[T]): Boolean
+  def tangents(other: Ray[T]): Boolean =
+    if (bound.isUnbounded || other.bound.isUnbounded) false
+    else if (isSameDirection(other)) false
+    else (bound, other.bound) match {
+      case (Closed(a), Open(b)) if a == b => true
+      case (Open(a), Closed(b)) if a == b => true
+      case _                              => false
+    }
 
   /**
    * Tests if this ray is connected to the other.  Rays are connected if they intersect, or are
@@ -58,36 +89,7 @@ sealed abstract class Ray[T] extends (T => Boolean) {
 
 case class GreaterRay[T: Ordering](bound: Bound[T]) extends Ray[T] with Ordered[GreaterRay[T]] {
 
-  override def apply(point: T): Boolean = bound match {
-    case Closed(value) => point >= value
-    case Open(value)   => point > value
-    case Unbounded()   => true
-  }
-
-  override def encloses(other: Ray[T]): Boolean =
-    if (bound.isUnbounded) true
-    else if (isSameDirection(other)) bound isBelow other.bound
-    else false
-
-  override def intersects(other: Ray[T]): Boolean =
-    if (bound.isUnbounded || other.bound.isUnbounded) true
-    else if (isSameDirection(other)) true
-    else (bound, other.bound) match {
-      case (Closed(a), Closed(b)) => a <= b
-      case (Closed(a), Open(b))   => a < b
-      case (Open(a), Closed(b))   => a < b
-      case (Open(a), Open(b))     => a < b
-      case _ => throw new AssertionError()
-    }
-
-  override def tangents(other: Ray[T]): Boolean =
-    if (bound.isUnbounded || other.bound.isUnbounded) false
-    else if (isSameDirection(other)) false
-    else (bound, other.bound) match {
-      case (Closed(a), Open(b)) if a == b => true
-      case (Open(a), Closed(b)) if a == b => true
-      case _                              => false
-    }
+  protected def directed: Ordering[T] = Ordering[T]
 
   override def tangent: Option[LesserRay[T]] = bound match {
     case Closed(p)   => Some(LesserRay(Open(p)))
@@ -118,36 +120,7 @@ case class GreaterRay[T: Ordering](bound: Bound[T]) extends Ray[T] with Ordered[
 
 case class LesserRay[T: Ordering](bound: Bound[T]) extends Ray[T] with Ordered[LesserRay[T]] {
 
-  override def apply(point: T): Boolean = bound match {
-    case Closed(value) => point <= value
-    case Open(value)   => point < value
-    case Unbounded()   => true
-  }
-
-  override def encloses(other: Ray[T]): Boolean =
-    if (bound.isUnbounded) true
-    else if (isSameDirection(other)) bound isAbove other.bound
-    else false
-
-  override def intersects(other: Ray[T]): Boolean =
-    if (bound.isUnbounded || other.bound.isUnbounded) true
-    else if (isSameDirection(other)) true
-    else (bound, other.bound) match {
-      case (Closed(a), Closed(b)) => a >= b
-      case (Closed(a), Open(b))   => a > b
-      case (Open(a), Closed(b))   => a > b
-      case (Open(a), Open(b))     => a > b
-      case _ => throw new AssertionError()
-    }
-
-  override def tangents(other: Ray[T]): Boolean =
-    if (bound.isUnbounded || other.bound.isUnbounded) false
-    else if (isSameDirection(other)) false
-    else (bound, other.bound) match {
-      case (Closed(a), Open(b)) if a == b => true
-      case (Open(a), Closed(b)) if a == b => true
-      case _ => false
-    }
+  protected val directed: Ordering[T] = Ordering[T].reverse
 
   override def tangent: Option[GreaterRay[T]] = bound match {
     case Closed(p)   => Some(GreaterRay(Open(p)))
